c7d070a213aec138c891e839a26621803b868ca4
Sandra Mora <105437004+KatsMoay@users.noreply.github.com>
2022-05-12 13:47:09 +0000

    Finalizado linea 7

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print('Hello World!')
 
 print ("Hola x2")
 
+print ("segundo mofulo de desarrrollo")
 
-
-print ("tercer mofulo de desarrrollo")
+print ("segundo mofulo de desarrrollo")
+print ("terfcer mofulo de desarrrollo")
